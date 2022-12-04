9b5f2d605c22a592ab4c02a1b00c6b363d9bd067
a100665 <tiagonunoteixeira@gmail.com>
2022-12-04 01:06:26 +0000

    Made it readable

--- day2/part2.c
+++ day2/part2.c
@@ -4,48 +4,27 @@
 //Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. If both players choose the same shape, the round instead ends in a draw.
 
 //the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a dra
-//, and Z means you need to win. Good luck!"
-
-
+//and Z means you need to win. Good luck!"
 // A  && X FOR ROCK
 // B && Y FOR PAPER 
 // C && Z FOR SCIZORS
+
 int calc_outcome (char opponent, char me) {
     int r = 0;
     if (opponent == 'A') {
-        printf ("Ele jogou pedra:%c\n",opponent);
-
-        if (me == 'X') { 
-            r += 3;    
-
-        }
-        else if (me == 'Y')  {
-        r += 4;
-        }
-         else {r+=8;}
+        if (me == 'X') r += 3;    
+        else if (me == 'Y') r += 4;
+        else r+=8;
     }
     else if (opponent == 'B') {
-        printf ("Ele jogou papel:%c\n",opponent);
-
-        if (me == 'X') {
-            r += 1;
-        }
-        else if (me == 'Y')  {
-        r += 5;
-        }
-            else if (me == 'Z') {r+=9;}
+        if (me == 'X') r += 1;
+        else if (me == 'Y') r += 5;
+        else r+=9;
     }
-    
     else if (opponent == 'C') {
-        printf ("Ele jogou tesoura:%c\n",opponent);
-
-        if (me == 'X') {
-            r += 2;
-        }
-        else if (me == 'Y')  {
-        r += 6;
-    }
-        else if (me == 'Z') {r+=7;}
+        if (me == 'X') r += 2;
+        else if (me == 'Y') r += 6;
+        else r+=7;
     }
 return r;
 }
@@ -53,30 +32,13 @@ return r;
 int  get_total_score (FILE * input) {
     char opponent;
     char status;
-    char * token;
     int total = 0;
-    int i = 0;
     size_t len;
     char * line = NULL;
-    while (getline( & line, & len, input) != -1 && !feof (input)) {
-        //char * line_aux = line;
-        //token = strsep( & line_aux, ";\n");
+    while (getline( & line, & len, input) != -1) {
         opponent = line[0];
         status = line[2];
-        printf ("Ele jogou:%c\n",opponent);
-        if (status == 'X') {
-        printf ("Devo perder:%c\n",status);
-        }
-            else if (status == 'Y') {
-             printf ("Devo empatar:%c\n",status);
-            }
-                else { 
-                     printf ("Devo ganhar:%c\n",status);
-                }
     total += calc_outcome (opponent,status);
-    printf ("Total_ronda%d:",i);
-    printf ("%d\n",total);
-    i++;
     }      
     return total;  
 }
